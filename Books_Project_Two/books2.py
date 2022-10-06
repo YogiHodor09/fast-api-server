ef00133697c384d14819552162468f3b2b5953b9
YogiPC\Yogeshwar <yogihodor09@outlook.com>
2022-10-06 19:55:23 +0530

    1. API for custom response model , headers , custom HTTP response code creation. 2. API for exception handling with decorators.
    Signed-off-by: YogiPC\Yogeshwar <yogihodor09@outlook.com>

--- Books_Project_Two/books2.py
+++ Books_Project_Two/books2.py
@@ -1,8 +1,17 @@
 from typing import Optional
 from uuid import UUID
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
+from starlette.responses import JSONResponse
+
+
+# Negative number exception
+
+class NegativeNumberException(Exception):
+    def __init__(self, book_to_return):
+        self.books_to_return = book_to_return
+
 
 app = FastAPI()
 
@@ -28,13 +37,45 @@ class Book(BaseModel):
         }
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title='Description of the book', max_length=100, min_length=1)
+
+
 BOOKS = []
+
+
+# Custom Exception Handler decorator
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request, exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={'message': f'Hey , Why you need {exception.books_to_return} books? You need to read more!'},
+    )
+
+
+# Created login for books page
+@app.post('/books/login')
+async def books_login(username: str = Form(), password: str = Form()):
+    return {'username': username, 'password': password}
+
+
+# Create Random Header in Request
+@app.get('/header')
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {'Random-Header': random_header}
 
 
 # Read all books if optional book number not give or
 # given as per requirement how many books wants to return
 @app.get('/')
 async def read_all_books(book_to_return: Optional[int] = None):
+    if book_to_return and book_to_return < 0:
+        raise NegativeNumberException(book_to_return=book_to_return)
+
     if len(BOOKS) < 1:
         create_books_no_api()
 
@@ -55,9 +96,20 @@ async def read_book(book_id: UUID):
     for x in BOOKS:
         if x.id == book_id:
             return x
+    raise raise_item_cannot_be_found_exception()
 
 
-@app.post('/')
+# Response Model with no_rating customised
+@app.get('/book/rating/{book_id}', response_model=BookNoRating)
+async def read_book_no_rating(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    raise raise_item_cannot_be_found_exception()
+
+
+@app.post('/',
+          status_code=status.HTTP_201_CREATED)  # Created custom response code HTTP_201_CREATED for success response
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
@@ -70,9 +122,11 @@ async def update_book(book_id: UUID, book: Book):
 
     for x in BOOKS:
         counter += 1
+        print(f'counter update value : {counter}')
         if x.id == book_id:
             BOOKS[counter - 1] = book
             return BOOKS[counter - 1]
+    raise raise_item_cannot_be_found_exception()
 
 
 @app.delete('/{book_id}')
@@ -80,9 +134,12 @@ async def delete_book(book_id: UUID):
     counter = 0
     for x in BOOKS:
         counter += 1
+        print(f'counter delete value : {counter}')
         if x.id == book_id:
             del BOOKS[counter - 1]
             return f'ID: {book_id} deleted'
+
+    raise raise_item_cannot_be_found_exception()
 
 
 # Create books list for dummy data to list books initially.
@@ -100,3 +157,8 @@ def create_books_no_api():
     BOOKS.append(book_2)
     BOOKS.append(book_3)
     BOOKS.append(book_4)
+
+
+def raise_item_cannot_be_found_exception():
+    return HTTPException(status_code=404, detail='Book not found!',
+                         headers={'X-Header-Error': 'Nothing to be seen at UUID'})
